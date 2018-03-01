cd81779f37c5223cd6ca4b1f59a52f2a3ae74c3c
Sicphy <petrov.kirill.1998@mail.ru>
2018-03-01 03:45:22 +0300

    zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+    var count = 0;
+    while(number > 1) {
+        number /=5;
+        number = parseInt(number);
+        count  += number;
+    }
+    return count;
 }
